a560dfc82073cc6bcf0b584ec40fe46edced62b5
Jeff Gonzalez <likwid@gmail.com>
2016-06-30 17:19:40 -0500

    Add vpc

--- main.tf
+++ main.tf
@@ -64,3 +64,13 @@ module "defaults" {
   region = "${var.region}"
   cidr   = "${var.cidr}"
 }
+
+module "vpc" {
+  source             = "./vpc"
+  name               = "${var.name}"
+  cidr               = "${var.cidr}"
+  internal_subnets   = "${var.internal_subnets}"
+  external_subnets   = "${var.external_subnets}"
+  availability_zones = "${var.availability_zones}"
+  environment        = "${var.environment}"
+}
